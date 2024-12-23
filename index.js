4df21b18c38549925741c006d0446dc64a5517b3
Karolis <karolisgied@gmail.com>
2024-12-23 23:40:17 +0200

    Update getComputerChoice function to return 'rock', 'paper', 'scissors'

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function getComputerChoice(){
-    return Math.ceil(Math.random()*3);
+    let rand = Math.ceil(Math.random()*3);
+    if(rand == 1)return 'rock';
+    if(rand == 2)return 'paper';
+    if(rand == 3)return 'scissors';
 }
-
