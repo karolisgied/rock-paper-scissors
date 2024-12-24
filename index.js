a317d07b7a70bf9fd91258287739ff9f9f3267cd
Karolis <karolisgied@gmail.com>
2024-12-24 19:02:22 +0200

    Add playRound function to play one round

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ function getHumanChoice() {
     if(choice == 3)return 'scissors';
 }
 
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice == computerChoice){
+        console.log(humanChoice ,' ties ',computerChoice);
+    }else if(humanChoice == 'rock' && computerChoice == 'scissors'){
+        console.log(humanChoice, ' beats ' , computerChoice);
+        humanScore++;
+    }else if(humanChoice == 'paper' && computerChoice == 'rock'){
+        console.log(humanChoice, ' beats ', computerChoice);
+        humanScore++;
+    }else if(humanChoice == 'scissors' && computerChoice == 'paper'){
+        console.log(humanChoice, ' beats ', computerChoice);
+        humanScore++;
+    }else if(computerChoice == 'rock' && humanChoice == 'scissors'){
+        console.log(computerChoice, ' beats ', humanChoice);
+        computerScore++;
+    }else if(computerChoice == 'paper' && humanChoice == 'rock'){
+        console.log(computerChoice, ' beats ', humanChoice);
+        computerScore++;
+    }else if(computerChoice == 'scissors' && humanChoice == 'paper'){
+        console.log(computerChoice, ' beats ', humanChoice);
+        computerScore++;
+    }
+}
